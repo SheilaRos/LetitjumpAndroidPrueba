305c1984d2c418a0ffbd8aeb0224167e1b39bc07
BilalHubani <bilal.hubani@gmail.com>
2017-01-12 19:03:54 +0100

    esbozo de la pantalla de inicio con fadeout

--- app/src/main/java/com/example/billy/jumpit/MainActivity.java
+++ app/src/main/java/com/example/billy/jumpit/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.billy.jumpit;
 
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -14,13 +23,44 @@ public class MainActivity extends Activity {
 // comando para que no aparezca el titulo
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 // comando para que sea en pantalla completa
-      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                  WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
 
         setContentView(R.layout.activity_main);
-
+//crear animacion slideout
+        final Animation slideout;
+        slideout = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        slideout.setDuration(500);
+//crear animacion fadeout
+        final Animation fadeout;
+        fadeout = AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        fadeout.setDuration(500);
+//crear variables del menu
+        final ImageButton play = (ImageButton) findViewById(R.id.play);
+        final TextView title = (TextView)findViewById(R.id.Title);
+        final TextView coins = (TextView)findViewById(R.id.coins);
+        final TextView diamonds = (TextView)findViewById(R.id.diamonds);
+        final ImageView coins_image = (ImageView)findViewById(R.id.coin_image);
+        final ImageView diamonds_image = (ImageView)findViewById(R.id.diamonds_image);
+//crear listener del play
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //usar animacion fadeout para los elementos del menu
+                play.startAnimation(fadeout);
+                play.setVisibility(View.INVISIBLE);
+                title.startAnimation(fadeout);
+                title.setVisibility(View.INVISIBLE);
+                coins.startAnimation(fadeout);
+                coins.setVisibility(View.INVISIBLE);
+                coins_image.startAnimation(fadeout);
+                coins_image.setVisibility(View.INVISIBLE);
+                diamonds.startAnimation(fadeout);
+                diamonds.setVisibility(View.INVISIBLE);
+                diamonds_image.startAnimation(fadeout);
+                diamonds_image.setVisibility(View.INVISIBLE);
+            }
+        });
     }
-
-
 }
 
