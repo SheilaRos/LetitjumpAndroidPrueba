297bd759ced251445ebae6dc59d47f377b3a2561
Bilal Hubani <bilal.hubani@gmail.com>
2017-01-16 18:01:16 +0100

    api 19

--- app/src/main/java/com/example/billy/jumpit/MainActivity.java
+++ app/src/main/java/com/example/billy/jumpit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.billy.jumpit;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -10,6 +12,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.concurrent.Delayed;
 
 
 public class MainActivity extends Activity {
@@ -29,25 +33,30 @@ public class MainActivity extends Activity {
         audio.load(this);
         audio.startMusic();
     }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View decorView = getWindow().getDecorView();
-// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-// comando para que no aparezca el titulo
        // requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 // comando para que sea en pantalla completa
-  //      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-      //          WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
-
+    /*    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);//int flag, int mask
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS,WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+*/
         setContentView(R.layout.activity_main);
 
 //crear animacion slideout
