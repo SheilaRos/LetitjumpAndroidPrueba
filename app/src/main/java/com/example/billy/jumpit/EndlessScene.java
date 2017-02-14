3e5c06aa99fd5e4deed2557ce73007c23d032e8f
BilalHubani <bilal.hubani@gmail.com>
2017-02-14 19:21:29 +0100

    primer codigo para endless

--- app/src/main/java/com/example/billy/jumpit/EndlessScene.java
+++ app/src/main/java/com/example/billy/jumpit/EndlessScene.java
@@ -10,10 +10,35 @@ public class EndlessScene {
     private String [][] mapa = new String[16][80];
 
     public String[][] updateMap(){
+        int platforms = 0;
+        int groundCounter = 0;
+
+        // buscamos info
         for (int i = 0; i<16;i++){
             for (int j = 0; j<80;j++){
-               // if (mapa[][])
+                // contamos las plataformas
+                if (mapa[i][j].equals("-"/* plataforma */) && mapa[i][j+1].equals("." /* cielo */)){
+                    platforms++;
+                }
+                // contamos el suelo
+                if (i == 16 && mapa[16][j].equals("_")){
+                    groundCounter++;
+                }
             }
+        }
+        // avanzamos el mapa una posicion
+        for (int i = 0; i<16;i++){
+            for (int j = 0; j<80;j++){
+                if (j>1)
+                    mapa[i][j-1] = mapa [i][j];
+            }
+        }
+        // pintamos la ultima linea
+        for (int i = 0; i<16;i++){
+            if (i == 16 && groundCounter<5)
+                mapa[i][80] = "_";
+
+            mapa[i][80] = ".";
         }
         return mapa;
     }
