15c86827cd3a44b5b6e0578341501c83f68f0c4c
kimsb1994 <kimsb1994@gmail.com>
2017-05-25 18:01:30 +0200

    #3 menu - completando el menu

--- app/src/main/java/com/example/billy/jumpit/ClassLevel.java
+++ app/src/main/java/com/example/billy/jumpit/ClassLevel.java
@@ -1,6 +1,7 @@
 package com.example.billy.jumpit;
 
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 /**
  * Created by lolrol1 on 14/5/17.
@@ -10,10 +11,12 @@ public class ClassLevel {
 
     private String nombre;
     private ImageButton button;
+    private TextView textView;
 
-    public ClassLevel(String nombre, ImageButton button) {
+    public ClassLevel(String nombre, ImageButton button, TextView textView) {
         this.nombre = nombre;
         this.button = button;
+        this.textView = textView;
     }
 
     public ClassLevel() {
