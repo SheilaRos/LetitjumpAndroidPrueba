d4280ba98b5c2f79620d13b289ae8e1c54b67a7d
SheilaRos <sheila.ros.cheme@gmail.com>
2017-05-26 18:16:46 +0200

    Falta probar conexion con backend

---
+++ app/src/main/java/com/example/billy/jumpit/util/CustomProperties.java
@@ -0,0 +1,9 @@
+package com.example.billy.jumpit.util;
+
+public class CustomProperties {
+    public static String clientId = "letitjump";
+    public static String clientSecret = "my-secret-token-to-change-in-production";
+    public static String grantType = "password";
+    public static String scope = "read write";
+    public static String baseUrl = "http://192.168.1.41:8282";
+}
